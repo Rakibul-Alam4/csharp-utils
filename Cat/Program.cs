4c3213d1bd8307214fcd43323e225975cc2179db
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-09 12:29:53 +0600

    Updated

---
+++ Cat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c1= new Cat();
+            c1.age = 10;
+            c1.name = "ASh";
+            c1.Colour = "ghm";
+
+            Console.WriteLine("age is " +c1.age +" name is " +c1.name+ " and colour is  "+c1.Colour);
+          c1.getInfo();
+            c1.run();
+            c1.wait();
+        }
+    }
+}
+
+
