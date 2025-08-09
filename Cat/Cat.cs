e7aa60d34aa5d3b1c67b52be9c501de3efb523d1
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-09 12:26:13 +0600

    Add files via upload

---
+++ Cat/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat
+{
+    public class Cat
+    {
+        public int age;
+        public string name;
+        public String Colour;
+        public Cat()
+        {
+            age = 10;
+            name = "Peu";
+            Colour = "White";
+
+        }
+        public Cat (int age, String name, String colour)
+        {
+            this.age = age;
+            this.name = name;
+            this.Colour = colour;
+        }
+        public void run()
+        {
+            Console.WriteLine("Running................");
+        }
+        public void wait()
+        {
+            Console.WriteLine("waiting................");
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("age is {0} name is {1} and colour is{2}", age,name,Colour);
+        }
+    }
+}
+
