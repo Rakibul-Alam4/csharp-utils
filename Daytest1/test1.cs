87b1346fa5cf98b013c1c010ed9237372e644d43
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-10 11:00:52 +0600

    Add files via upload

---
+++ Daytest1/test1.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daytest1
+{
+    internal class test1
+    {
+        static void Main(string[] args)
+        {
+            /* Console.Write("Enter num1:");
+             int num1 = Convert.ToInt32 (Console.ReadLine());
+             Console.Write("Enter num2:");
+             int num2 = Convert.ToInt32(Console.ReadLine());
+
+             int addition = num1 + num2;
+             Console.WriteLine("\n The sum is:" +addition);
+
+             int sub = num1 - num2;
+             Console.WriteLine("The Sub is:" + sub);
+            */
+           /*
+            Console.Write("Enetr the name:");
+            String name= Console.ReadLine(); ; 
+            Console.Write("Enter the marks:");
+            int marks= Convert.ToInt32(Console.ReadLine());
+            String Status;
+            if (marks >= 50)
+            {
+                Status = "Passed";
+            }
+            else
+            {
+                Status = "Failed";
+            }
+            Console.WriteLine("Hi " +name + " you have "+ Status);
+             */ 
+
+           /*
+            Console.Write("Enter the hight:");
+            int high = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the weight:");
+            int weigt = Convert.ToInt32(Console.ReadLine());
+
+            double radius = 1.8 * high*weigt;
+            Console.WriteLine("The redus is: " + radius);
+           */
+
+
+        }
+    }
+}
