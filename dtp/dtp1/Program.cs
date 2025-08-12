86e6805b0cefb8b5e08bc26b7219e9d983a8ff06
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-12 11:33:24 +0600

    Add files via upload

---
+++ dtp/dtp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace dtp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("Enter the number: ");
+            string input = Console.ReadLine();
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+
+            string reverse = new string(charArray);
+            Console.WriteLine("Reversed : " + reverse);
+            */
+
+            Console.Write("Enter the message: ");
+            string input = Console.ReadLine();
+
+           string Output = " ";
+            for (int i = 0; i < input.Length; i++)
+            {
+                Output += input[i];
+                if (i < input.Length - 1)
+                    Output += " ";
+            }
+            Console.WriteLine("Output: "+Output);
+
+        }
+    }
+}
