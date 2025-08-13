08b69ead7a5013f96cb7d7ab5eb0a17ddbf2691f
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-13 11:17:21 +0600

    Add files via upload

---
+++ IntroC#/IntroC#/Intro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroC_
+{
+    internal class Intro
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Word");
+            //Console.ReadKey();  
+
+            String message = "hello word";
+                Console.WriteLine(message);
+            message = "hello";
+            Console.WriteLine(message);
+            message = "world";
+               Console.WriteLine(message);
+                Console.ReadKey();
+        
+        }
+    }
+}
