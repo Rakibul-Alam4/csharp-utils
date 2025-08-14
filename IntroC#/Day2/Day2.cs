090b5790ab7a930d43df4a6f9734c21fbd117d46
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-14 10:54:46 +0600

    Add files via upload

---
+++ IntroC#/Day2/Day2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Day2
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("This is Day2");
+            //Console.ReadKey();
+
+            /*
+            Console.WriteLine("Enter the number");
+            Console.Write("Num1 is:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Num 2 is: ");
+            int num2= Convert.ToInt32(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine("The result is :" + result);
+            */
+
+            Console.Write("Enter your name:");
+            String name= Console.ReadLine();
+
+            Console.Write("Enter the marks:");
+            int marks = Convert.ToInt32(Console.ReadLine());
+            string Status;
+            if (marks > 50)
+            {
+                Status = "Passed";
+            }
+            else
+            {
+                Status = "Failed";
+            }
+            Console.WriteLine("Hi " +name+  " you have "  + Status);
+
+            Console.ReadKey();
+        }
+    }
+}
