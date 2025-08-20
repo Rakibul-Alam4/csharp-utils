98498a2fedf22f801b40a0e61688fc47366e1bb1
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-08-20 11:10:29 +0600

    Add files via upload

---
+++ Space between each characte/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_between_each_characte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /*
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string output = string.Join(" ", input.ToCharArray());
+
+        Console.WriteLine("Output: " + output);
+    }
+        */
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                output += input[i];
+                if (i < input.Length - 1) // Avoid adding a trailing space
+                    output += " ";
+            }
+
+            Console.WriteLine("Output: " + output);
+        }
+    }
+}
